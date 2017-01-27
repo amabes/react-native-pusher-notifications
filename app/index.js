9220cbc0827a31c1ddfe1c15d9b1752234d47ef0
Alan Mabry <alan.mabry@gmail.com>
2017-01-27 14:40:38 -0800

    use component life cycle to log value of picker

--- app/index.js
+++ app/index.js
@@ -1,5 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Picker, AppState } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    this.state = {
+      seconds: 10
+    }
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  }
+
+  handleAppStateChange(appState) {
+    if (appState === 'background') {
+      // #TODO : Sched. background notification
+    }
+    console.log('app is in the background', this.state.seconds);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Choose notification time (secs)</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={this.state.seconds}
+          onValueChange={(seconds) => this.setState({ seconds })}
+        >
+          <Picker.Item label="5" value={5} />
+          <Picker.Item label="10" value={10} />
+          <Picker.Item label="15" value={15} />
+        </Picker>
+      </View>
+
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -18,16 +62,7 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello!
-        </Text>
-      </View>
-    );
+  picker: {
+    width: 100
   }
-}
+});
